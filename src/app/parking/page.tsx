3cae6313e8fc027d7c774fe02749b9560f00d100
Mariel <mendozamarieljoy@gmail.com>
2026-04-05 21:34:07 +0800

    Removed scheduler controls on bottom of the page

--- src/app/parking/page.tsx
+++ src/app/parking/page.tsx
@@ -3,7 +3,6 @@
 import Calendar from "@/components/Calendar";
 import FairnessDashboard from "@/components/FairnessDashboard";
 import CostSimulationPanel from "@/components/CostSimulationPanel";
-import SchedulerControls from "@/components/SchedulerControls";
 import Header from "@/components/Header";
 
 export default function ParkingPage() {
@@ -19,12 +18,9 @@ export default function ParkingPage() {
 
       <div className="space-y-4 p-6">
         <Calendar />
-        <div>
-          <div className="flex flex-wrap lg:grid grid-cols-2 gap-4 mb-4">
-            <FairnessDashboard />
-            <CostSimulationPanel />
-          </div>
-          <SchedulerControls />
+        <div className="flex flex-wrap lg:grid grid-cols-2 gap-4 mb-4">
+          <FairnessDashboard />
+          <CostSimulationPanel />
         </div>
       </div>
     </>
