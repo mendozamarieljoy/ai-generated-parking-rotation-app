e3905fb92abd6638e57fa53ee706faaea1c8069c
Mariel <mendozamarieljoy@gmail.com>
2026-05-13 21:34:44 +0800

    [HOLIDAY] Add May 27 as holdiday

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -23,6 +23,7 @@ export const holidays = [
   { month: 1, day: 1, holidayName: "New Year's Day" },
   { month: 4, day: 9, holidayName: "Araw ng Kagitingan" },
   { month: 5, day: 1, holidayName: "Labor Day" },
+  { month: 5, day: 27, holidayName: "Eid'l Fitr" },
   { month: 6, day: 12, holidayName: "Independence Day" },
   { month: 8, day: 31, holidayName: "National Heroes Day" },
   { month: 11, day: 30, holidayName: "Bonifacio Day" },
@@ -32,5 +33,4 @@ export const holidays = [
   // TO BE CONFIRMED - MOVING HOLIDAYS
   // { month: 3, day: 25, holidayName: "Maundy Thursday" },
   // { month: 3, day: 26, holidayName: "Good Friday" },
-  // { month: 3, day: 20, holidayName: "Eid'l Fitr" },
 ];
